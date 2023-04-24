a92ac4f0d4311590e2241274ece8fd3c4e0d64c7
XhmikosR <xhmikosr@gmail.com>
2023-04-24 19:49:07 +0300

    Update all the things
    * Drop Node.js < 12 support
    * Remove debug dependency
    * Fix tests on Windows
    * Switch to GitHub Actions CI
    * Add c8 for coverage
    * Update dependencies
    * Replace mocha with uvu
    * Add xo for linting

--- test/test.js
+++ test/test.js
@@ -1,83 +1,121 @@
-var assert = require('assert');
-var mock = require('mock-fs');
-var lookup = require('../');
+'use strict';
 
-describe('stylus-lookup', function() {
-  beforeEach(function() {
-    mock({
-      example: {
-        // jscs: disable maximumLineLength
-        'main.styl': '@import "blueprint"; @require "another"; @require "styles.styl"',
-        // jscs: enable maximumLineLength
-        'another.styl': '@import "nested/foo"',
-        'styles.styl': '@import "styles2.css"',
-        'styles2.css': '',
-        blueprint: {
-          'index.styl': ''
-        },
-        nested: {
-          'foo.styl': ''
-        }
+const path = require('path');
+const process = require('process');
+const mock = require('mock-fs');
+const { suite } = require('uvu');
+const assert = require('uvu/assert');
+const lookup = require('../index.js');
+
+const testSuite = suite('stylus-lookup');
+
+testSuite.before.each(() => {
+  mock({
+    example: {
+      'main.styl': '@import "blueprint"; @require "another"; @require "styles.styl"',
+      'another.styl': '@import "nested/foo"',
+      'styles.styl': '@import "styles2.css"',
+      'styles2.css': '',
+      blueprint: {
+        'index.styl': ''
+      },
+      nested: {
+        'foo.styl': ''
       }
-    });
+    }
   });
-
-  afterEach(mock.restore);
-
-  it('handles index.styl lookup', function() {
-    assert.equal(lookup({
-      dependency: 'blueprint',
-      filename: 'example/main.styl',
-      directory: 'example'
-    }),
-      process.cwd() + '/example/blueprint/index.styl');
-  });
-
-  it('handles .css lookups', function() {
-    assert.equal(lookup({
-      dependency: 'styles2.css',
-      filename:  'example/styles.styl',
-      directory: 'example'
-    }),
-      process.cwd() + '/example/styles2.css');
-  });
-
-  it('handles same directory lookup', function() {
-    assert.equal(lookup({
-      dependency: 'another',
-      filename: 'example/main.styl',
-      directory: 'example'
-    }),
-      process.cwd() + '/example/another.styl');
-  });
-
-  it('handles subdirectory lookup', function() {
-    assert.equal(lookup({
-      dependency: 'nested/foo',
-      filename:  'example/another.styl',
-      directory: 'example'
-    }),
-      process.cwd() + '/example/nested/foo.styl');
-  });
-
-  it('handles extensionless lookup', function() {
-    assert.equal(lookup({
-      dependency: 'another',
-      filename: 'example/main.styl',
-      directory: 'example'
-    }),
-      process.cwd() + '/example/another.styl');
-  });
-
-  it('handles extensioned lookup', function() {
-    assert.equal(lookup({
-      dependency: 'styles.styl',
-      filename:  'example/main.styl',
-      directory: 'example'
-    }),
-      process.cwd() + '/example/styles.styl');
-  });
-
-  it.skip('supports globbing imports');
-  it.skip('supports additional path lookups');
 });
+
+testSuite.after.each(() => mock.restore);
+
+testSuite('throws if dependency is not supplied', () => {
+  assert.throws(() => lookup({
+    filename: 'example/baz.styl',
+    directory: 'example'
+  }), Error, 'dependency is not a supplied');
+});
+
+testSuite('throws if filename is not supplied', () => {
+  assert.throws(() => lookup({
+    dependency: 'blueprint',
+    directory: 'example'
+  }), Error, 'filename is not a supplied');
+});
+
+testSuite('throws if directory is not supplied', () => {
+  assert.throws(() => lookup({
+    dependency: 'blueprint',
+    filename: 'example/baz.styl'
+  }), Error, 'directory is not a supplied');
+});
+
+testSuite('handles index.styl lookup', () => {
+  const expected = path.normalize(`${process.cwd()}/example/blueprint/index.styl`);
+  const actual = lookup({
+    dependency: 'blueprint',
+    filename: 'example/styles.styl',
+    directory: 'example'
+  });
+
+  assert.is(actual, expected);
+});
+
+testSuite('handles .css lookups', () => {
+  const expected = path.normalize(`${process.cwd()}/example/styles2.css`);
+  const actual = lookup({
+    dependency: 'styles2.css',
+    filename: 'example/styles.styl',
+    directory: 'example'
+  });
+
+  assert.is(actual, expected);
+});
+
+testSuite('handles same directory lookup', () => {
+  const expected = path.normalize(`${process.cwd()}/example/another.styl`);
+  const actual = lookup({
+    dependency: 'another',
+    filename: 'example/main.styl',
+    directory: 'example'
+  });
+
+  assert.is(actual, expected);
+});
+
+testSuite('handles subdirectory lookup', () => {
+  const expected = path.normalize(`${process.cwd()}/example/nested/foo.styl`);
+  const actual = lookup({
+    dependency: 'nested/foo',
+    filename: 'example/another.styl',
+    directory: 'example'
+  });
+
+  assert.is(actual, expected);
+});
+
+testSuite('handles extensionless lookup', () => {
+  const expected = path.normalize(`${process.cwd()}/example/another.styl`);
+  const actual = lookup({
+    dependency: 'another',
+    filename: 'example/main.styl',
+    directory: 'example'
+  });
+
+  assert.is(actual, expected);
+});
+
+testSuite('handles extensioned lookup', () => {
+  const expected = path.normalize(`${process.cwd()}/example/styles.styl`);
+  const actual = lookup({
+    dependency: 'styles.styl',
+    filename: 'example/main.styl',
+    directory: 'example'
+  });
+
+  assert.is(actual, expected);
+});
+
+testSuite.skip('supports globbing imports');
+testSuite.skip('supports additional path lookups');
+
+testSuite.run();
